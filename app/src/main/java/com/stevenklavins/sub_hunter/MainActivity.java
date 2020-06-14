7de5bd5275cb58ccee15d65b1eb46b9b4d658c11
Steven <stevenklavins94@gmail.com>
2020-06-14 16:52:31 +0100

    Feat: spiked methods required for game

--- app/src/main/java/com/stevenklavins/sub_hunter/MainActivity.java
+++ app/src/main/java/com/stevenklavins/sub_hunter/MainActivity.java
@@ -3,6 +3,7 @@ package com.stevenklavins.sub_hunter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void newGame () {
+
+    }
+
+    void draw () {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    void takeShot () {
+
+    }
+
+    void boom () {
+
+    }
+
+    void  printDebugging () {
+
     }
 }
