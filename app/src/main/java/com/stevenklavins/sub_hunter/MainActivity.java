2f1eddc94203b1f1838c6c6c0aaffdd49aca43fd
Steven <stevenklavins94@gmail.com>
2020-06-14 17:00:08 +0100

    chore: added needed packages

--- app/src/main/java/com/stevenklavins/sub_hunter/MainActivity.java
+++ app/src/main/java/com/stevenklavins/sub_hunter/MainActivity.java
@@ -1,9 +1,18 @@
 package com.stevenklavins.sub_hunter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.Display;
+import android.util.Log;
+import android.widget.ImageView;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
